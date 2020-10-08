5c3e61972a8f51b6e1f63aed27a2d875e81cabeb
Vladarc <vladmironecdonnu@gmail.com>
2020-10-08 19:40:10 +0300

    new

---
+++ precache-manifest.bdf93f62a85d349887150f929a3dc278.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69c4467ee156103bc2b1c4e289159199",
+    "url": "/betaviz/index.html"
+  },
+  {
+    "revision": "37226eba3e2173948908",
+    "url": "/betaviz/static/css/main.abdc5d7f.chunk.css"
+  },
+  {
+    "revision": "05a3edeb3290b34731c6",
+    "url": "/betaviz/static/js/2.a97035aa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/betaviz/static/js/2.a97035aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37226eba3e2173948908",
+    "url": "/betaviz/static/js/main.2295db71.chunk.js"
+  },
+  {
+    "revision": "b26dc516427c8eea0896",
+    "url": "/betaviz/static/js/runtime-main.37416524.js"
+  },
+  {
+    "revision": "b42ee0d6fc2a466e41a083ec4dbde422",
+    "url": "/betaviz/static/media/businesscard_square-original-square_corners-glosslam-square.b42ee0d6.png"
+  },
+  {
+    "revision": "73d77e57107afedef21d2cf8bae63a0b",
+    "url": "/betaviz/static/media/businesscard_us-original-square_corners-glosslam.73d77e57.png"
+  },
+  {
+    "revision": "19887db32f73e5a40e3fe2ceb7c3a52f",
+    "url": "/betaviz/static/media/businesscard_us-original-square_corners-mattelam.19887db3.png"
+  },
+  {
+    "revision": "48f147ee5ed29078e6077337543ca194",
+    "url": "/betaviz/static/media/gloss-original-card.48f147ee.jpg"
+  },
+  {
+    "revision": "5c49ac2f4e52ab911182b3a2110768ad",
+    "url": "/betaviz/static/media/logo.5c49ac2f.jpg"
+  },
+  {
+    "revision": "22e5d0e28a1101a03cb9e8035f39b985",
+    "url": "/betaviz/static/media/originaCard.22e5d0e2.jpg"
+  },
+  {
+    "revision": "97af6ff7b45f3f99ccdb1859a3cdf320",
+    "url": "/betaviz/static/media/upload-card.97af6ff7.jpg"
+  }
+]);
